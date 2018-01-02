21996fa8007d221a57f90d2bedb4a461f9676890
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-01-02 12:39:57 +0100

    fetchat ut huvudkategorier json

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 $(document).ready(function(){ 
+
+    var huvudKat
+
+    //Tar upp informationen från JSON-filen
+    fetch("json/huvudkategorier.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        huvudKat = data;
+        console.log(huvudKat);
+
+        for(i = 0; i < huvudKat.length; i++) {
+
+            console.log(huvudKat[i].id);
+            console.log(huvudKat[i].teacolor);
+            $('#huvudKat').append('<li class="dropdown" id='+ i +'><a class="dropdown-toggle" data-toggle="dropdown" href="#">'+ huvudKat[i].teacolor +'<span class="caret"></span></a><ul class="dropdown-menu" id="underKat"></ul></li>');
+
+
+            };
+
+    
+    });
+    //stängt fetchen
+
+    
+
+
+
 });
